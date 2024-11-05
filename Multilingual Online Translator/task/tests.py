5b321f1e50f7fd8b045a3385c0a870f0d5f2374e
roblebob <robert.koppisch@gmail.com>
2024-11-05 10:53:07 +0100

    Initial commit

---
+++ Multilingual Online Translator/task/tests.py
@@ -0,0 +1,3 @@
+from test.tests import TranslatorTest
+
+if __name__ == '__main__':    TranslatorTest('translator.translator').run_tests()
---
+++ Multilingual Online Translator/task/translator/translator.py
@@ -0,0 +1,7 @@
+print('Type "en" if you want to translate from French into English, or "fr" if you want to translate from English into French:')
+target_language = input()
+
+print('Type the word you want to translate:')
+word = input()
+
+print(f'You chose {target_language} as a language to translate {word}.')
